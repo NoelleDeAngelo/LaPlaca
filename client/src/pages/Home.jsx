a809e7f95ab5c14e02881077fae9cf6a7ff1887a
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-09-30 13:29:31 -0700

    Add background img to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 class Home extends React.Component {
   render(){
     return(
-      <div>
-        <h1>Home</h1>
+      <div id='home-page'>
+        <div className= 'brand-name'>LaPlaca</div>
       </div>
     );
   }
