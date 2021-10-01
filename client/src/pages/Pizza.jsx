8fa266f89eff83617d92b327c191be12a28f1316
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-09-30 17:54:32 -0700

    Add Modal to pizza page for pizza order form

--- client/src/pages/Pizza.jsx
+++ client/src/pages/Pizza.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Modal from 'react-modal';
 
 
 class Pizza extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      modalIsOpen: false,
+    }
+  }
+
+
   render(){
     return(
       <div>
         <h1>Pizza</h1>
+        <button onClick= {()=> this.setState({modalIsOpen:true})}>Order Pizza</button>
+        <Modal isOpen ={this.state.modalIsOpen}
+         onRequestClose= {()=> this.setState({modalIsOpen:false})}
+          style={{
+          'overlay': {'background':'grey'},
+          'content': {'color':'black', 'width': '450px', 'margin':'auto'} }}>
+             <span style={{'float': 'right', 'fontSize': '150%'}} onClick= {()=> this.setState({modalIsOpen:false}) }>&#10006;</span>
+             <form name= 'pizza-order' id= 'pizza-order-form'>
+              <h2>Order Your Pizza</h2>
+              <label for = 'customer-first-name'>First Name: </label>
+               <input type= 'text' name= 'customer-first-name' required></input> <br></br>
+               <label for = 'customer-last-name'>Last Name: </label>
+               <input type= 'text' name= 'customer-last-name' required></input>
+             </form>
+          </Modal>
       </div>
     );
   }
