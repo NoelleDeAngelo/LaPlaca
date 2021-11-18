bb5de98de30dec5ae14df5397135c4c0eda9fd66
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-11-18 15:19:26 -0800

    Add cards for each pizza type to pizza page and remove order pizza modal

--- client/src/components/PizzaDes.jsx
+++ client/src/components/PizzaDes.jsx
@@ -10,13 +10,42 @@ class PizzaDes extends React.Component {
 
   render(){
     let pizzaDescription = {
-      fourCheese: 'A pizza with every cheese imaginable, if you only imagine four cheeses',
-      veg: 'A garden on a pizza!',
-      byo: 'Choose your toppings below',
+      cheese:{
+        name: 'Four Cheese',
+        des:'A pizza with every cheese imaginable, if you only imagine four cheeses.',
+        ingredients: [],
+        price: 18.00,
+        picture: '../../dist/PeperoniPizza.jpg',
+      } ,
+      veg:{
+        name: 'Vegetable',
+        des: 'A garden on a pizza!',
+        ingredients:[],
+        price: 25.00,
+        picture: '../../dist/VegPizza.jpg',
+      },
+      byo: {
+        name:'Build Your Own',
+        des:'Pick your toppings',
+        ingredients:[],
+        price: 10.00,
+        picture: '../../dist/ByoPizza.jpg'
+      },
       none: 'Please select a pizza'
     };
+
+    let pizza = pizzaDescription[this.props.type];
+
     return(
-      <p>{pizzaDescription[this.props.type]}</p>
+      <div className = 'pizza-card flex-column'>
+        <img src= {pizza.picture} ></img>
+        <div className= 'pizza-card-text'>
+          <h3>{pizza.name}</h3>
+          <p>{pizza.des}</p>
+        </div>
+
+      </div>
+
     );
   }
 }
--- client/src/pages/Pizza.jsx
+++ client/src/pages/Pizza.jsx
@@ -17,44 +17,62 @@ class Pizza extends React.Component {
   }
 
 
-  render(){
-    return(
-      <div className = 'pizza-page'>
-        <h1><u>Pizza</u></h1>
-        <button id ='order-button' onClick= {()=> this.setState({modalIsOpen:true})}>Order A Pizza</button>
-        <Modal isOpen ={this.state.modalIsOpen}
-         onRequestClose= {()=> this.setState({modalIsOpen:false, pizzaSelected: 'none'})}
-          style={{
-          'overlay': {'background':'grey'},
-          'content': {'color':'black', 'width': '450px', 'margin':'auto'} }}>
-             <span style={{'float': 'right', 'fontSize': '150%'}} onClick= {()=> this.setState({modalIsOpen:false, pizzaSelected: 'none'}) }>&#10006;</span>
-             <form name= 'pizza-order' id= 'pizza-order-form'>
-              <h2>Order Your Pizza</h2>
-              <label for = 'customer-first-name'>First Name: </label>
-               <input type= 'text' name= 'customer-first-name' required></input> <br></br>
-               <label for = 'customer-last-name'>Last Name: </label>
-               <input type= 'text' name= 'customer-last-name' required></input>
-               <p> Pizza Type: </p>
-               <div>
-                <input onClick = {()=> {this.changePizzaSelected('fourCheese')}} type= 'radio' id= 'four-cheese' name = 'pizza-type' value = 'four-cheese'></input>
-                <label for = 'four-cheese'> Four Cheese </label>
-               </div>
-              <div>
-                <input onClick = {()=> {this.changePizzaSelected('veg')}}type= 'radio' id= 'veg' name = 'pizza-type' value = 'veg'></input>
-                <label for = 'veg'> Veggie </label>
-              </div>
-              <div>
-                <input onClick = {()=> {this.changePizzaSelected('byo')}} type= 'radio' id= 'byo' name = 'pizza-type' value = 'byo'></input>
-                <label for = 'byo'> Build Your Own </label>
-              </div>
-              <PizzaDes type= {this.state.pizzaSelected}/>
+//   render(){
+//     return(
+//       <div className = 'pizza-page'>
+//         <h1><u>Pizza</u></h1>
+//         <button id ='order-button' onClick= {()=> this.setState({modalIsOpen:true})}>Start Your Order</button>
+//         <Modal isOpen ={this.state.modalIsOpen}
+//          onRequestClose= {()=> this.setState({modalIsOpen:false, pizzaSelected: 'none'})}
+//           style={{
+//           'overlay': {'background':'grey'},
+//           'content': {'color':'black', 'width': '450px', 'margin':'auto'} }}>
+//              <span style={{'float': 'right', 'fontSize': '150%'}} onClick= {()=> this.setState({modalIsOpen:false, pizzaSelected: 'none'}) }>&#10006;</span>
+//              <form name= 'pizza-order' id= 'pizza-order-form'>
+//               <h2><u>Order Your Pizza</u></h2>
+//               <label for = 'customer-first-name'>First Name: </label>
+//                <input type= 'text' name= 'customer-first-name' required></input> <br></br>
+//                <label for = 'customer-last-name'>Last Name: </label>
+//                <input type= 'text' name= 'customer-last-name' required></input>
+//                <p> Pizza Type: </p>
+//                <div>
+//                 <input onClick = {()=> {this.changePizzaSelected('fourCheese')}} type= 'radio' id= 'four-cheese' name = 'pizza-type' value = 'four-cheese'></input>
+//                 <label for = 'four-cheese'> Four Cheese </label>
+//                </div>
+//               <div>
+//                 <input onClick = {()=> {this.changePizzaSelected('veg')}}type= 'radio' id= 'veg' name = 'pizza-type' value = 'veg'></input>
+//                 <label for = 'veg'> Veggie </label>
+//               </div>
+//               <div>
+//                 <input onClick = {()=> {this.changePizzaSelected('byo')}} type= 'radio' id= 'byo' name = 'pizza-type' value = 'byo'></input>
+//                 <label for = 'byo'> Build Your Own </label>
+//               </div>
+//               <PizzaDes type= {this.state.pizzaSelected}/>
 
 
-             </form>
-          </Modal>
+//              </form>
+//           </Modal>
+//       </div>
+//     );
+//   }
+// }
+
+render(){
+  return(
+    <div className = 'pizza-page '>
+      <h1><u>Pizzas</u></h1>
+      <div className= 'pizzas-list flex-row'>
+        <PizzaDes type= 'byo'/>
+        <PizzaDes type= 'cheese'/>
+        <PizzaDes type= 'veg'/>
+        <PizzaDes type= 'cheese'/>
+        <PizzaDes type= 'veg'/>
+        <PizzaDes type= 'byo'/>
       </div>
-    );
-  }
+
+    </div>
+  );
+}
 }
 
 export default Pizza;
