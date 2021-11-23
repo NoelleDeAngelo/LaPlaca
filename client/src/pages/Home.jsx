f14ff1ee9113745e6009c0006baf78e410841b98
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-11-23 10:54:20 -0800

    Update Bakery Information

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,13 +6,13 @@ class Home extends React.Component {
     return(
       <div id='home-page'>
         <div id= 'name-box'>
-          <p className= 'brand-name' id= 'home-brand-name'>LaPlaca</p>
+          <p className= 'brand-name' id= 'home-brand-name'>LaBlaca</p>
           <p id= 'home-tagline'>Family Bakery and Pizzeria</p>
         </div>
         <div id= 'location-box'>
           <p><u>Location</u></p>
-          <p id= 'location'>9280 highway 9 <br/>
-            Ben Lomond, CA, CA 95005</p>
+          <p id= 'location'>80 Highway 9 <br/>
+            Ben Lomond, CA 95005</p>
         </div>
       </div>
     );
