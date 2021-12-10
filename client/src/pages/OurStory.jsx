e186384e9ffe06cc6b30589e0d3170bade9b0849
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2021-12-09 16:02:59 -0800

    Add "About Us" text to OurStory

--- client/src/pages/OurStory.jsx
+++ client/src/pages/OurStory.jsx
@@ -6,6 +6,7 @@ class OurStory extends React.Component {
     return(
       <div>
         <h1>OurStory</h1>
+        <p>LaBlaca bakery was founded by Fred LaBlaca. Fred was passionate about sharing his love of baking with the world. In 1947, he opened the bakery with his son, Albert. They were one of the first commercial bakeries in the United States. The bakery opened with a few employees, but the number grew as the business grew. Today, the company owns and operates over 20 shops throughout the country. The family continues to stay involved in business, and the love they have for their customers is evident in each day. </p>
       </div>
     );
   }
